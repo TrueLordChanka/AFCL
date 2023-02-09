a456e3e321a41dcaf29abc83ebceaf75af95fcde
Andrew_FTW <andrewftw01@gmail.com>
2023-02-08 21:23:33 -0500

    Added caseless and bipods

---
+++ AFCL/BetterCaseless.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using FistVR;
+
+
+namespace AndrewFTW
+{ 
+    public class BetterCaseless : MonoBehaviour
+    {
+        private FVRFireArmRound Round;
+
+#if !(UNITY_EDITOR || UNITY_5 )
+
+        public void Awake()
+        {
+            Round = this.gameObject.GetComponent<FVRFireArmRound>();
+            //Debug.Log(this.gameObject.name + " is a caseless round");
+        }
+
+        public void FixedUpdate()
+        {
+            //Debug.Log(Round.name + " is a caseless round and is spent? " + Round.m_isSpent);
+            if (Round.m_isSpent) // the other one is m_isSpent, idk if it works different.
+            {
+                Destroy(Round.gameObject);
+            }
+        }
+
+
+
+#endif
+    }
+}
+
+
+
---
+++ AFCL/BipodExtras.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using FistVR;
+
+
+namespace AndrewFTW
+{ 
+    public class BipodExtras : MonoBehaviour
+    {
+        public FVRFireArmBipod Bipod;
+        public bool Rotatelegs;
+        public GameObject LegHolder1;
+        public GameObject LegHolder2;
+        public Vector3 LegOpenAngles1;
+        public Vector3 LegOpenAngles2;
+
+        public bool MoveObj;
+        public GameObject ObjToMove;
+        public Vector3 ObjOpenLocalPos;
+
+
+        private bool m_beenUpdated = false;
+
+#if !(UNITY_EDITOR || UNITY_5 )
+
+        
+
+        public void Update()
+        {
+            //Debug.Log(Round.name + " is a caseless round and is spent? " + Round.m_isSpent);
+            if (Bipod.m_isBipodExpanded && !m_beenUpdated) //if its expanded and this is the first time its seeing this 
+            {
+                if (Rotatelegs)
+                {
+                    LegHolder1.transform.localEulerAngles = LegOpenAngles1;
+                    LegHolder2.transform.localEulerAngles = LegOpenAngles2;
+                }
+                if (MoveObj)
+                {
+                    ObjToMove.transform.localPosition = ObjOpenLocalPos;
+                }
+
+                m_beenUpdated = true;
+            }
+
+            if (!Bipod.m_isBipodExpanded && m_beenUpdated)
+            {
+                if (Rotatelegs)
+                {
+                    LegHolder1.transform.localEulerAngles = new Vector3 (0,0,0);
+                    LegHolder2.transform.localEulerAngles = new Vector3(0, 0, 0);
+                }
+                if (MoveObj)
+                {
+                    ObjToMove.transform.localPosition = new Vector3(0, 0, 0);
+                }
+
+                m_beenUpdated = false;
+            }
+
+        }
+
+
+
+#endif
+    }
+}
+
+
+
